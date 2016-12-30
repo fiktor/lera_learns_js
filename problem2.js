158ea29ddc2136f30292974540bdffab5615a82c
Victor Kasatkin <vi_kasatkin@inbox.ru>
2016-12-30 15:01:10 -0800

    problem2 solution

--- problem2.js
+++ problem2.js
@@ -1,58 +1,88 @@
-function IsNumber(c) {
+function IsDigit(c) {
   const code = c.charCodeAt(0);
   return '0'.charCodeAt(0) <= code && code <= '9'.charCodeAt(0);
 }
 
-function CheckProblem(problem) {
+function CheckProblemLera(problem) {
   const l = problem.length;
-    
+
+  let n = 0;
+  let m = 0;
   for (let i = 0; i < l; i++) {
-    if (problem[i] !== "+" && problem[i] !== "-" && problem[i] !== "(" && problem[i] !== ")" && problem[i] !== " " && !IsNumber(problem[i])) {
-      
+    if (problem[i] !== "+" && problem[i] !== "-" && problem[i] !== "(" &&
+        problem[i] !== ")" && problem[i] !== " " && !IsDigit(problem[i])) {
       return false;
     }
-    var n = 0;
-    var m = 0;
-    for (let i = 0; i < l; i++) {
-      if (problem[i] == "(") {
-        n = n+1;
-        
-      }
+    if (problem[i] == "(") {
+      n++;
     }
-    for (let i = 0; i < l; i++) {
-      if (problem[i] == ")") {
-        m = m+1;
-        
-      }
+    if (problem[i] == ")") {
+      m++;
     }
-      
-      
-    
-    
-   if (m !== n) {
+  }
+
+  if (m !== n) {
+    return false;
+  } 
+
+  let pos_open = -1;
+  let pos_closed = -1;
+  for (let i = 0; i < n; i++) {
+    for (pos_open++; problem[pos_open] !== '('; pos_open++) {
+      // Do nothing
+    }
+    for (pos_closed++; problem[pos_closed] !== ')'; pos_closed++) {
+      // Do nothing
+    }
+    if (pos_open > pos_closed) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function CheckProblemVictor(problem) {
+  let num_open_brackets = 0;
+  let state = '(';
+  // state:
+  // * '(' --- we can expect anything we could expect after '('.
+  // * '1' --- we can expect anything we could expect after '1'.
+  // * ')' --- we can expect anything we could expect after ')'.
+
+  for (let pos = 0; pos < problem.length; pos++) {
+    let cur = problem[pos];
+    if (cur === '(') {
+      num_open_brackets++;
+    } else if (cur === ')') {
+      num_open_brackets--;
+      if (num_open_brackets < 0) {
         return false;
-    } 
-      
-      
-    for (let i = 0; i < l; i++) {
-      if (problem[i] == "(") {
-        n = i;
-          console.log(i);
-       for (let j = 0; j < l; j++) {
-         if (problem[j] == ")") {
-             m = j;
-             console.log(j);
-         }
-       }
-          if (n < m) {
-              return true;
-          }
-          return false;
+      }
+    }
+
+    if (state === '(') {
+      if (cur === ' ' || cur === '(') {
+        state = '(';
+      } else if (IsDigit(cur)) {
+        state = '1';
+      } else {
+        return false;
+      }
+    } else {
+      // state === ')' || state === '1'
+      if (IsDigit(cur) && state === '1') {
+        state = '1';
+      } else if (cur === ' ' || cur === ')') {
+        state = ')';
+      } else if (cur === '+' || cur === '-') {
+        state = '(';
+      } else {
+        return false;
       }
     }
   }
-    
-    return true;
+  return num_open_brackets === 0 && (state === '1' || state === ')');
 }
 
 function CheckProblem2(solution) {
@@ -65,6 +95,11 @@ function CheckProblem2(solution) {
     {input: '((((((((((2)))))))))', answer: false},
     {input: '1 + 3) + (4 + 7', answer: false},
     {input: '(1 + 3) + (4 + 7)', answer: true},
+    {input: '(1 + 3)) + ((4 + 7)', answer: false},
+    {input: '(1 + )', answer: false},
+    {input: ' + ', answer: false},
+    {input: '', answer: false},
+    {input: '1 + ()', answer: false},
     {input: '49       +      3', answer: true}];
   for (let i = 0; i < examples.length; i++) {
     const example = examples[i];
@@ -81,4 +116,4 @@ function CheckProblem2(solution) {
   return true;
 }
 
-CheckProblem2(CheckProblem);
+CheckProblem2(CheckProblemVictor);
